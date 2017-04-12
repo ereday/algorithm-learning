4896d7441c9c29c0a353c8641c8db0ef88ac0f6c
okirnap <okirnap@balina.ku.edu.tr>
2017-04-12 13:48:09 +0300

    finisy copy_data

---
+++ data_generator.jl
@@ -0,0 +1,44 @@
+# data generation file
+# action set:
+# "mr" -> move right
+
+const complexities = Dict{AbstractString, Int}("copy"=>1,
+                                               "reverse"=>2,
+                                               "walk"=>1,
+                                               "add"=>2,
+                                               "radd"=>3,
+                                               "mul"=>1)
+const goldacts = Dict{Symbol, AbstractString}(:moveright=>"mr",
+                                             :moveleft=>"mr",
+                                             :up=>"up",
+                                             :down=>"down")
+
+"""
+experiment is a string given as lowercase name
+"""
+function gendata(experiment::AbstractString)
+    !haskey(complexities, experiment) && error("wrong key type $experiment")
+
+    comp = complexities[experiment]
+    fname = string(experiment, "_data")
+    f = eval(parse(fname))
+    return f
+end
+
+
+function copy_data(seqlen::Int)
+    data = Any[ rand(0:9) for i=1:seqlen ]
+    actions = [ goldacts[:moveright] for i =1:seqlen ] # to do decide the last item
+    ygold = data
+    return (data, ygold, actions)
+end
+
+
+function reverse_data(seqlen::Int)
+    data = Any[ rand(0:9) for i=1:seqlen ]
+    ygold = reverse(data)
+    push!(ygold, -1)
+    push!(data, "r")
+    actions = [ goldacts[:moveright] for i=1:seqlen ]
+    return (data, ygold, actions)
+end
